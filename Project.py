bdc1ee76859faaff0cb6151f571fa1cbe629bf2f
wierz009 <wierz009@rangers.uwp.edu>
2024-04-11 15:33:35 -0500

    updated matricies.py and Project.py

--- Project.py
+++ Project.py
@@ -10,8 +10,9 @@ def generate_ellipse(center, x, y, angle, num_points=100):
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
     ellipse_points = np.array([a * cos_theta, b * sin_theta])
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
-                                [np.sin(angle), np.cos(angle)]])
+    #rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
+    #                            [np.sin(angle), np.cos(angle)]])
+    rotation_matrix = np.array([[1,0], [0,1]])
     rotated_points = np.dot(rotation_matrix, ellipse_points)
     return rotated_points[0] + center[0], rotated_points[1] + center[1]
     return center[0], center[1]
@@ -21,8 +22,8 @@ def generate_ellipse(center, x, y, angle, num_points=100):
 def update(frame):
 
     # Calculate new position of the moving point
-    x = center[0] + np.cos(np.radians(frame)) * radius
-    y = center[0] + np.sin(np.radians(frame)) * radius
+    x = center[0] + np.cos(np.radians(frame)) * a
+    y = center[0] + np.sin(np.radians(frame)) * b
     moving_point.set_data(x, y)
 
     # Update the ellipse
